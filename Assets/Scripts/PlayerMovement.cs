686bab3545acf2a35f1a9e7f76122f31ac4df92d
Gamar <gamarazuaje@gmail.com>
2021-01-29 14:04:44 +0900

    Added Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidBody;
+    private Vector3 change;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero; 
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if (change != Vector3.zero){
+            Movecharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+
+        }
+
+        Debug.Log(change);   
+    }
+
+    void Movecharacter(){
+        myRigidBody.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+        );
+
+    }
+
+}
