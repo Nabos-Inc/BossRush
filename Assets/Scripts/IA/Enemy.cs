39b6fc436dff824e1a623c9ea9ac918aa532c10f
Kevin <kevin_mena3@hotmail.com>
2021-01-30 01:41:10 -0400

    Bolt added and some scripts for the enemies

---
+++ Assets/Scripts/IA/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField] protected int health;
+
+    [SerializeField] protected float baseSpeed;
+
+    [SerializeField] protected int baseDamage;
+
+    protected Rigidbody2D rb2d;
+
+    public void Init()
+    {
+        rb2d =  GetComponent<Rigidbody2D>();
+    }
+
+    public abstract void Move(Vector3 direction);
+
+    public abstract void Attack();
+}
---
+++ Assets/Scripts/IA/KnightController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightController : Enemy
+{
+    void Awake()
+    {
+        base.Init();
+    }
+
+    public override void Move(Vector3 direction)
+    {
+        rb2d.MovePosition(transform.position + direction * baseSpeed * Time.deltaTime);
+    }
+
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+}
