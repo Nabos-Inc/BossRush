457f3108b257b716ceda9a7c7431f95cb4d53c56
Gamar <gamarazuaje@gmail.com>
2021-01-31 18:20:42 +0900

    Added New Bosses

---
+++ Assets/Scripts/Player/Unlockable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unlockable : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody2D;
+    public GameObject dialogBox;
+    public string unlockableName; 
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogBox.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        Debug.Log("Unlocked item!");
+        if (other.CompareTag("Player")){
+            Destroy(gameObject);
+            dialogBox.SetActive(true);
+        }
+
+    }
+}
