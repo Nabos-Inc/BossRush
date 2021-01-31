34b5d4250459831c751aafa33424f14b30941bfd
Kevin <kevin_mena3@hotmail.com>
2021-01-31 02:34:39 -0400

    Boss scene ready

--- Assets/Scripts/IA/KnightController.cs
+++ Assets/Scripts/IA/KnightController.cs
@@ -10,15 +10,19 @@ public class KnightController : Enemy
 
     public override void Move(Vector3 direction)
     {
-        direction.Normalize();
+        //direction.Normalize();
 
-        if(Mathf.Abs(direction.y) > 0.001f && Mathf.Abs(direction.x) > 0.001f)
+        if(Mathf.Abs(direction.y) > 0.1f && Mathf.Abs(direction.x) > 0.1f)
         {
             direction.x = 0f;
         }
-        else if(Mathf.Abs(direction.x) > 0.001f)
+        else if(Mathf.Abs(direction.x) > 0.1f)
         {
             direction.y = 0f;
+        }
+        else if(Mathf.Abs(direction.y) > 0.1f)
+        {
+            direction.x = 0f;
         }
 
         Vector2 movementVector = new Vector2(GetRawValue(direction.x), GetRawValue(direction.y));
